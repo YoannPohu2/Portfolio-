d14889c602858c40a521e73e817638424cb44092
YoannPohu2 <153502409+YoannPohu2@users.noreply.github.com>
2025-04-02 21:22:49 +0200

    Init project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import AboutMe from './Components/AboutMe';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+      <AboutMe />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../css/Navbar.css";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-left">
+                <h1>Mon portfolio</h1>
+            </div>
+            <div className="navbar-right">
+                <ul>
+                    <li><a href="#accueil">Home</a></li>
+                    <li><a href="#a-propos">About me</a></li>
+                    <li><a href="#parcours">Mon parcours</a></li>
+                    <li><a href="#competences">Compétences</a></li>
+                    <li><a href="#projets">Mes projets</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
