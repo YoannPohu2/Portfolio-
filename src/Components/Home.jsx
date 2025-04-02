106562397eb66f4d6f50bc047a305f36e4bf80b0
YoannPohu2 <153502409+YoannPohu2@users.noreply.github.com>
2025-04-02 21:31:32 +0200

    Add Home section

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <div class="home">
             <img src={profilePhoto} alt="Yoann Pohu" class="profile-photo" />
             <h1>Yoann Pohu</h1>
-            <p>Full Stack Developer</p>
+            <p>Full Stack web Developer</p>
             <div className="links">
                 <a href="https://github.com/your-github" target="_blank" rel="noopener noreferrer">GitHub</a>
                 <a href="https://linkedin.com/in/your-linkedin" target="_blank" rel="noopener noreferrer">LinkedIn</a>
